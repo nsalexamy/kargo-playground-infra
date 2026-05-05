a74fb55c43f7eb2a311802eca53e31522b875904
Young Kim <young.kim@teck.com>
2026-05-05 12:06:31 -0700

    update clean up script

--- utilities/Provider-Usage-Export.ps1
+++ utilities/Provider-Usage-Export.ps1
@@ -1,20 +1,22 @@
 # Pass pkg_suffix as an argument to the script, e.g.:
-# .\Provider-Usage-Export.ps1 -provider_suffix "grafana.crossplane.io"
+# .\Provider-Usage-Export.ps1 -provider_keyword "grafana.crossplane.io"
 param (
-    [string]$provider_suffix
+    [string]$provider_keyword
 )
-Write-Output "Exporting usage for provider suffix: $provider_suffix"
+Write-Output "Exporting usage for provider suffix: $provider_keyword"
 
 # Error handling for missing argument
-if (-not $provider_suffix) {
-    Write-Host "Please provide a provider suffix as an argument, e.g. -provider_suffix 'grafana.crossplane.io'."
+if (-not $provider_keyword) {
+    Write-Host "Please provide a provider suffix as an argument, e.g. -provider_keyword 'grafana'."
     exit
 }
 
 $results = @()
+$legacyProvider = "${provider_keyword}.crossplane.io"
+
 
 # search all CRDs with the specified provider suffix and extract usage information
-kubectl get crds -o name | Where-Object { $_ -like "*$provider_suffix*" } | ForEach-Object {
+kubectl get crds -o name | Where-Object { $_ -like "*$legacyProvider*" } | ForEach-Object {
     #Write-Output "Processing CRD: $_"
 
     # trim until '/' to get the CRD name
@@ -30,9 +32,9 @@ kubectl get crds -o name | Where-Object { $_ -like "*$provider_suffix*" } | ForE
 # Output
 
 if ( $results.Count -eq 0 ) {
-    Write-Output "No results found for provider suffix '$provider_suffix'."
-    Write-Output "Empty result will be saved to provider_usage_$provider_suffix.json."
-    $results | ConvertTo-Json -Depth 100 | Out-File -FilePath "provider_usage_$provider_suffix.json" -Encoding utf8
+    Write-Output "No results found for provider suffix '$provider_keyword'."
+    Write-Output "Empty result will be saved to provider_usage_$provider_keyword.json."
+    $results | ConvertTo-Json -Depth 100 | Out-File -FilePath "provider_usage_$provider_keyword.json" -Encoding utf8
 } else {
-    $results | ConvertTo-Json -Depth 100 | Out-File -FilePath "provider_usage_$provider_suffix.json" -Encoding utf8
+    $results | ConvertTo-Json -Depth 100 | Out-File -FilePath "provider_usage_$provider_keyword.json" -Encoding utf8
 }
